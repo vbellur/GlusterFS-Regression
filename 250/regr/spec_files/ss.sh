5728766b4ce46cae41d293e7511c6430c9707dac
Vijay Bellur <vijay@gluster.com>
2010-01-26 20:46:09 +0530

    More changes.

--- 250/regr/spec_files/ss.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-(for j in {1..10} ; do for i in {1..10} ; do touch file$i ; setfattr -n trusted.user -v 2  file$i ; rm file$i ; done ; done)& > /dev/null
-PID1=$!
-
-(for k in {1..1000} ; do getfattr -n trusted.user * ; done)
-
-kill $PID1
